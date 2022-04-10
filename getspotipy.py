c2539bfae54cfeb11e8ac8c092c6135fa2e7a1b3
Spin Eve <20128868+SpinEve@users.noreply.github.com>
2022-04-10 15:11:45 -0400

    Read client info from file.

--- getspotipy.py
+++ getspotipy.py
@@ -1,6 +1,7 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
+import os
 
 def get_track(sp, albumId):
     res = sp.album_tracks(albumId)
@@ -15,18 +16,22 @@ def get_feature(sp, trackIds):
     sp.audio_features(trackIds)
 
 if __name__ == '__main__':
-    clientId = input('Client id: ')
-    clientSec = input('Client secret: ')
-    # clientId = ''
-    # clientSec = ''
+    if os.path.exists('client.txt'):
+        with open('client.txt', 'r') as f:
+            clientId = f.readline()
+            clientSec = f.readline()
+    else:
+        clientId = input('Client id: ')
+        clientSec = input('Client secret: ')
 
     authMang = SpotifyClientCredentials(clientId, clientSec)
     sp = spotipy.Spotify(auth_manager=authMang)
 
-    trackNames, trackIds = get_track(sp, input('Album id: '))
+    albumId = input('Album id: ')
+    trackNames, trackIds = get_track(sp, albumId)
 
     res = sp.audio_features(trackIds)
     df = pd.DataFrame.from_dict(res)
 
     df.insert(0, 'trackname', trackNames)
-    df.to_csv('album.csv')
+    df.to_csv('album_%s.csv' % albumId)
