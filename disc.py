095eadb2b533f963d56d897c148bebb3fee02dd5
Grayson <101232737+graysonftaylor@users.noreply.github.com>
2022-04-11 14:24:15 -0400

    Added title & platform

--- disc.py
+++ disc.py
@@ -1,12 +1,11 @@
 import bpy
+import bpy.ops as o
 import bmesh
 import math
 import mathutils
 import numpy as np
 import requests
-import bpy.ops as o
-
-bm = bmesh.new()
+# Authors: Eric, Taishan, Grayson
 
 # prints output to console
 def print(data):
@@ -16,27 +15,70 @@ def print(data):
             if area.type == 'CONSOLE':
                 override = {'window': window, 'screen': screen, 'area': area}
                 bpy.ops.console.scrollback_append(override, text=str(data), type="OUTPUT")
+      
+#random generators
+def rand():
+  return (np.random.rand()-0.5)*2
+def randsq():
+  return (np.random.rand()**2-0.5)*2
+      
+#cube creation
+def cube(loc, resize):
+  o.mesh.primitive_cube_add(location = loc)
+  o.transform.resize(value = resize)
 
-#clears
-def objclear():
-  for obj in bpy.context.scene.objects:
-    if obj.type == 'MESH':
-      obj.select = True
+#prism creation
+def prism(loc, resize, vert, angle, angle2):
+  o.mesh.primitive_cylinder_add(vertices = vert, location = loc)
+  ob = bpy.context.object
+  ob.rotation_euler=(0, 0, angle)
+  o.transform.resize(value = resize)
+  ob.rotation_euler=(0, 0, angle2)
+    
+#creates platform with edge number = valence, text = title string
+def createPlatform(valence):
+    #round platform
+    if valence > 0.8:
+        bpy.ops.mesh.primitive_torus_add(location = (0,0,0), major_radius=5, minor_radius = 0.1, major_segments = 256)
+        bpy.ops.mesh.primitive_cone_add(radius1 = 5.3, radius2 = 5, depth=0.5, location = (0,0,-0.2), vertices = 1000)
+    #octagon platform
+    elif valence > 0.6:
+        prism((0,0, -0.2),(5.5,5.5,0.2), 8, 3, 1)
+        prism((0,0, -0.2),(5.5,5.5,0.2), 8, 3, 1)
+    #hexagon platform
+    elif valence > 0.4:
+        prism((0,0, -0.2),(5.5,5.5,0.2), 6, 3, 1)
+    #pentagon platform
+    elif valence > 0.2:
+        prism((0,0, -0.2),(5.5,5.5,0.2), 5, 3, 1)
+    #square platform
     else:
-      obj.select = False
-      o.object.delete()
+        prism((0,0, -0.2),(6.5,6.5,0.2), 4, 3, 1)
+    bpy.ops.object.text_add(radius=1, location = (-1,-4.5,0),rotation=(1.5,0,0))
+    ob=bpy.context.object
+    ob.data.body = title
+    bpy.context.object.data.extrude = 0.1
+
         
 #testing print on a get
 response = requests.get('https://google.com/')
 print(response.status_code)
+
+
+bm = bmesh.new()
 
 rStep = 6
 maxR = 4.0
 minR = 0.5
 maxH = 1
 bottomH = 0.3
-
 segCount = 10
+
+#track values
+valence = 0.9
+energy = 0
+tempo = 0
+title = "title"
 
 vertsList = []
 
@@ -47,6 +89,9 @@ rList = np.linspace(minR, maxR, rStep)
 
 hArray = np.random.random((rStep, segCount)) * 0.4 + 0.2
 scaleSeg = np.random.random((segCount, )) * 0.1 + 0.95
+
+#creates platform with edge number determined by valence score
+createPlatform(valence)
 
 # Create circle verts
 for i in range(rStep):
@@ -80,7 +125,6 @@ for i in range(segCount):
 bottomOutVerts = bottomOutMesh["verts"]
 bottomInVerts = bottomInMesh["verts"]
 
-
 # Outer face
 outVerts = vertsList[rStep-1]
 
@@ -109,6 +153,3 @@ bm.free()
 
 obj = bpy.data.objects.new("Object", me)
 bpy.context.collection.objects.link(obj)
-
-# bpy.ops.mesh.primitive_cylinder_add(
-#    vertices=segCount, radius=minR, depth=2)  # Bottom
