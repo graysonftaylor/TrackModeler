15680f4fed27f4b34835d91af52465e583a3024c
Spin Eve <20128868+SpinEve@users.noreply.github.com>
2022-04-11 22:44:15 -0400

    Update waves.

--- disc_single.py
+++ disc_single.py
@@ -5,7 +5,7 @@ import mathutils
 import numpy as np
 
 
-MAX_DURATION = 300000
+MAX_DURATION = 360000
 
 SEG_COUNT = 64
 
@@ -19,9 +19,9 @@ H1 = 0.2
 rStep = 6
 
 loudness = -4.843
-tempo = 100
+tempo = 200
 duration = 249627
-energy = 0.8
+energy = 0.3
 valence = 0.567
 danceability = 0.617
 
@@ -29,8 +29,8 @@ danceability = 0.617
 def outerRing(bm, ring1_d, ring1_u, ring2_d, ring2_u):
     # Outer circle: Duration
 
-    seg = int(np.floor((duration / MAX_DURATION) * SEG_COUNT))
-    
+    seg = int(np.floor((duration / MAX_DURATION) * SEG_COUNT)) + 1
+
     print(seg)
 
     for i in range(seg):
@@ -67,18 +67,77 @@ def disk(bm, ring0_d, ring0_u, ring1_d, ring1_u):
 def waves(bm):
     maxAng = 2*np.pi * duration / MAX_DURATION
 
-    pointCnt = int(np.floor(tempo / 4))
+    pointCnt = int(np.ceil(tempo / 8))
+
+    thetaList = np.linspace(0, maxAng, pointCnt)
+    
+    print(pointCnt)
+
+    HList = np.zeros((rStep, pointCnt))
+
+    # Energy - Amplifier of height
+    baseH = H1 + (loudness + 10) / 20
+    maxAmp = energy / 2
+
+    for i in range(rStep):
+        for j in range(pointCnt):
+            HList[i, j] = baseH + (np.random.random() - 0.5) * maxAmp
+
+    rList = np.linspace(R0+0.1, R1-0.2, rStep)
+    fan = int(np.floor(pointCnt * (duration / MAX_DURATION)))
+    
+    print(fan, duration / MAX_DURATION)
+
+    baseCircleOut = bmesh.ops.create_circle(
+        bm, segments=pointCnt, radius=R1)['verts'][:fan+1]
+
+    bmesh.ops.translate(bm, verts=baseCircleOut, vec=(0, 0, H1))
+
+    baseCircleIn = bmesh.ops.create_circle(
+        bm, segments=pointCnt, radius=R0+0.1)['verts'][:fan+1]
+
+    bmesh.ops.translate(bm, verts=baseCircleIn, vec=(0, 0, H1))
+
+    circles = []
+
+    for i in range(rStep):
+        tCircle = bmesh.ops.create_circle(
+            bm, segments=pointCnt, radius=rList[i])['verts'][:fan+1]
+        for j in range(fan+1):
+            bmesh.ops.translate(
+                bm, verts=[tCircle[j]], vec=(0, 0, HList[i, j]))
+        # bmesh.ops.smooth_laplacian_vert(bm, verts=tCircle)
+        circles.append(tCircle)
+
+    for r in range(rStep-1):
+        for i in range(fan):
+            bm.faces.new([circles[r][i], circles[r][i+1],
+                         circles[r+1][i+1], circles[r+1][i]])
+
+    for i in range(fan):
+        bm.faces.new([circles[0][i], circles[0][i+1],
+                      baseCircleIn[i+1], baseCircleIn[i]])
+        bm.faces.new([circles[rStep-1][i], circles[rStep-1][i+1],
+                      baseCircleOut[i+1], baseCircleOut[i]])
+    
+    
+
+
+def waves2(bm):
+    maxAng = 2*np.pi * duration / MAX_DURATION
+
+    pointCnt = int(np.floor(tempo / 8))
 
     thetaList = np.linspace(0, maxAng, pointCnt)
 
-    HList = np.zeros((pointCnt, rStep))
+    HList = np.zeros((rStep, pointCnt))
 
     # Energy - Amplifier of height
     baseH = H1 + (loudness + 10) / 30
     maxAmp = energy / 4
 
-    for i in range(pointCnt):
-        for j in range(rStep):
+    for i in range(rStep):
+        for j in range(pointCnt):
             HList[i, j] = baseH + (np.random.random() - 0.5) * maxAmp
 
     rList = np.linspace(R0+0.1, R1-0.2, rStep)
@@ -86,12 +145,13 @@ def waves(bm):
 
     circles = []
 
-    for r in range(rStep):
+    for i in range(rStep):
         tCircle = bmesh.ops.create_circle(
-            bm, segments=pointCnt, radius=rList[r])['verts'][:fan]
-        for i in range(fan):
+            bm, segments=pointCnt, radius=rList[i])['verts'][:fan]
+        for j in range(fan):
             bmesh.ops.translate(
-                bm, verts=[tCircle[i]], vec=(0, 0, HList[i, r]))
+                bm, verts=[tCircle[j]], vec=(0, 0, HList[i, j]))
+        # bmesh.ops.smooth_laplacian_vert(bm, verts=tCircle)
         circles.append(tCircle)
 
     for r in range(rStep-1):
@@ -120,7 +180,7 @@ if __name__ == '__main__':
 
     if loudness < -10:
         loudness = -10
-    H2 = H1 + (loudness + 10) / 20
+    H2 = H1 + (loudness + 10) / 40
 
     # For outer ring
     ring1_u2 = bmesh.ops.create_circle(
@@ -138,6 +198,9 @@ if __name__ == '__main__':
     disk(bm, ring0_d, ring0_u, ring1_d, ring1_u)
 
     waves(bm)
+
+    # for f in bm.faces:
+    #     f.smooth = True
 
     me = bpy.data.meshes.new("Mesh")
     bm.to_mesh(me)
